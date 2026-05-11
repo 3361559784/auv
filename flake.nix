71dfcba61894ed5e74b7248e5d77ee1d1d3eea63
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-05-11 17:09:22 +0800

    chore: init

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "github:moeru-ai/auv";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs =
+              (with pkgs; [
+                # rust
+                rustc
+                cargo
+                rustfmt
+                clippy
+                rust-analyzer
+              ])
+              ++ [];
+
+            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+          };
+
+          packages = forAllSystems (
+            system:
+            let
+              pkgs = import nixpkgs { inherit system; };
+              version = (fromTOML (builtins.readFile ./Cargo.toml)).package.version;
+            in
+            {
+              default = pkgs.rustPlatform.buildRustPackage {
+                inherit version;
+                pname = "auv-cli";
+                src = ./.;
+                cargoLock.lockFile = ./Cargo.lock;
+              };
+            }
+          );
+        }
+      );
+    };
+}
